54e54bb5506cf1ceff2523b5557568b5febce99f
Eqedos <parthkumarj117@gmail.com>
2026-03-28 14:46:10 +0800

    feat: add programmatic P favicon

---
+++ src/app/icon.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#0a0a0a",
+        borderRadius: 6,
+      }}
+    >
+      <span
+        style={{
+          fontSize: 22,
+          fontFamily: "monospace",
+          fontWeight: 700,
+          color: "#a3a3a3",
+          letterSpacing: -1,
+        }}
+      >
+        P
+      </span>
+    </div>,
+    { ...size }
+  );
+}
